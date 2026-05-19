94419bf37b390b9e5d0bda9e25b48a5b433c3c86
mhnddbarbwd-ops <mhnddbarbwd@gmail.com>
2026-05-19 09:53:39 +0300

    إصلاح main.dart لنفحات

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:my_first_app/models/user_activity.dart';
-import 'package:my_first_app/models/user_profile.dart';
-import 'package:my_first_app/screens/dashboard_screen.dart'; // <-- تغيير هنا
-import 'package:my_first_app/theme/app_theme.dart';
-import 'package:my_first_app/services/notification_service.dart';
-import 'package:my_first_app/services/alarm_service.dart';
-import 'package:my_first_app/services/database_service.dart';
-import 'package:my_first_app/services/pedometer_service.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:hijri_date/hijri_date.dart';
+import 'package:nafahat/screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await Hive.initFlutter();
-  Hive.registerAdapter(UserActivityAdapter());
-  Hive.registerAdapter(UserProfileAdapter());
-  await Hive.openBox<UserProfile>('profileBox');
-  await Hive.openBox('settingsBox');
-  await DatabaseService().init();
-  await NotificationService().init();
-  PedometerService().startListening();
-  runApp(const FezApp());
+  await initializeDateFormatting('ar', null);
+  HijriDate.setLocal('ar');
+  runApp(const NafahatApp());
 }
 
-class FezApp extends StatefulWidget {
-  const FezApp({super.key});
-
-  @override
-  State<FezApp> createState() => _FezAppState();
-}
-
-class _FezAppState extends State<FezApp> {
-  ThemeMode _themeMode = ThemeMode.system;
-  String _language = 'ar';
-
-  void toggleTheme() {
-    setState(() {
-      _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
-    });
-  }
-
-  void toggleLanguage() {
-    setState(() {
-      _language = _language == 'ar' ? 'en' : 'ar';
-    });
-  }
+class NafahatApp extends StatelessWidget {
+  const NafahatApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AlarmService(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'فِـز',
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
-        themeMode: _themeMode,
-        locale: Locale(_language),
-        supportedLocales: const [Locale('ar'), Locale('en')],
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        home: DashboardScreen(), // <-- تغيير هنا
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'نفحات',
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: const Color(0xFF1B5E20),
+        scaffoldBackgroundColor: const Color(0xFFF5F0E8),
+        textTheme: GoogleFonts.ibmPlexSansArabicTextTheme(),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          titleTextStyle: GoogleFonts.ibmPlexSansArabic(
+            fontWeight: FontWeight.w900,
+            fontSize: 22,
+            color: const Color(0xFF1B5E20),
+          ),
+        ),
       ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: const Color(0xFF1B5E20),
+        textTheme: GoogleFonts.ibmPlexSansArabicTextTheme(
+          ThemeData.dark().textTheme,
+        ),
+      ),
+      home: const SplashScreen(),
     );
   }
 }
